d5cf4b278a812b7e29016d115eee1d4018464972
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-24 11:59:03 +0200

    - created form-field-ui class

---
+++ src/app/reactive-forms-inputs/classes/form-field-ui.spec.ts
@@ -0,0 +1,7 @@
+import { FormFieldUi } from './form-field-ui';
+
+describe('FormFieldUi', () => {
+  it('should create an instance', () => {
+    expect(new FormFieldUi()).toBeTruthy();
+  });
+});
---
+++ src/app/reactive-forms-inputs/classes/form-field-ui.ts
@@ -0,0 +1,9 @@
+import { FormField } from '../../shared/interfaces/form-field';
+
+export class FormFieldUI {
+  public formField: FormField;
+
+  constructor(formField: FormField) {
+    this.formField = formField;
+  }
+}
