c413df0ae5c769e73b70307304e84ab451594be5
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-24 16:27:44 +0200

    - implemented FormSectionUI

--- src/app/reactive-forms/classes/form-section-ui.ts
+++ src/app/reactive-forms/classes/form-section-ui.ts
@@ -1,2 +1,19 @@
+import { FormSection } from '../../shared/interfaces/form-section';
+import { FormFieldUI } from '../../reactive-forms-inputs/classes/form-field-ui';
+import { FormGroup } from '@angular/forms';
+
 export class FormSectionUI {
+
+  public formSection: FormSection;
+
+  public sectionDisplayId: string;
+  public fieldsUI: Array<FormFieldUI>;  
+  public sectionFormGroup: FormGroup;
+  public mainFormGroup: FormGroup;
+  //public parentFormSchemaUI: FormSchemaUI;
+
+  constructor(formSection: FormSection) {
+    this.formSection = formSection;
+    this.sectionDisplayId = 'section' + this.formSection.sectionId.toString();
+  }
 }
