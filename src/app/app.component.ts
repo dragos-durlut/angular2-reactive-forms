a7049526a842ed8ba381d5c39a3c4e91579ab606
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-24 18:00:54 +0200

    - setup app-main-form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormSchemaUI } from './shared/classes/form-schema-ui';
+import { FormSectionUI } from './shared/classes/form-section-ui';
+import { FormFieldUI } from './shared/classes/form-field-ui';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,30 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'reactive-forms';
+
+  public formSchemaUI: FormSchemaUI;
+  loading: boolean;
+  saving: boolean;
+
+  public fieldsToUpdate: any = {};
+
+
+  constructor() {
+    this.loading = true;
+  }
+
+  public saveFieldData = () => {
+
+    this.formSchemaUI.sectionsUI.forEach(function (sectionUI: FormSectionUI) {
+      sectionUI.fieldsUI.forEach(function (fieldUI: FormFieldUI) {
+        if (fieldUI.fieldFormControl.dirty) {
+          this.fieldsToUpdate[fieldUI.formField.fieldId] = fieldUI.formField.value;
+        }
+      });
+    });
+    
+      console.info("Fields were updated!");
+    //TODO: update some <pre> tag
+  }
+
 }
