7367ec35a530a77fa99e0505cfe64f05fb9f7c46
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-24 18:51:43 +0200

    - set FormFieldUiBuilder as a provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from './reactive-forms/reactive-forms.module';
+import { FormFieldUiBuilder } from './reactive-forms-inputs/builders/form-field-ui-builder';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { ReactiveFormsModule } from './reactive-forms/reactive-forms.module';
     BrowserAnimationsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [FormFieldUiBuilder ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
