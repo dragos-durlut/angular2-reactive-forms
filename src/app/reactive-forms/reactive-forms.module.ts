4db59861aeacaa0e5a02c36c73c6cfa215e34db2
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-24 17:26:55 +0200

    - created main-form component

--- src/app/reactive-forms/reactive-forms.module.ts
+++ src/app/reactive-forms/reactive-forms.module.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { ReactiveFormsInputsModule } from '../reactive-forms-inputs/reactive-forms-inputs.module';
 import { LabelWithFieldTupleComponent } from './label-with-field-tuple/label-with-field-tuple.component';
 import { FormSectionComponent } from './form-section/form-section.component';
+import { MainFormComponent } from './main-form/main-form.component';
 
 
 
 @NgModule({
-  declarations: [LabelWithFieldTupleComponent, FormSectionComponent],
+  declarations: [LabelWithFieldTupleComponent, FormSectionComponent, MainFormComponent],
   imports: [
     CommonModule,
     ReactiveFormsInputsModule
