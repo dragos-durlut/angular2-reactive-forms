58ecbc10d3bf9ec0018d62785419de14b6a16715
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-24 15:42:44 +0200

    - created label-with-field component

---
+++ src/app/reactive-forms/label-with-field-tuple/label-with-field-tuple.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-label-with-field-tuple',
+  templateUrl: './label-with-field-tuple.component.html',
+  styleUrls: ['./label-with-field-tuple.component.scss']
+})
+export class LabelWithFieldTupleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/reactive-forms/reactive-forms.module.ts
+++ src/app/reactive-forms/reactive-forms.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsInputsModule } from '../reactive-forms-inputs/reactive-forms-inputs.module';
+import { LabelWithFieldTupleComponent } from './label-with-field-tuple/label-with-field-tuple.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [LabelWithFieldTupleComponent],
   imports: [
     CommonModule,
     ReactiveFormsInputsModule
