3e334cbc5011fbfdedc8a11fa59af3168d0b8d70
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-24 16:20:38 +0200

    - created form-section-ui class

---
+++ src/app/reactive-forms/classes/form-section-ui.spec.ts
@@ -0,0 +1,7 @@
+import { FormSectionUI } from './form-section-ui';
+
+describe('FormSectionUi', () => {
+  it('should create an instance', () => {
+    expect(new FormSectionUI()).toBeTruthy();
+  });
+});
