23fc70b8801ba86cf27b035999f9bb62f7fa751c
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-24 17:22:39 +0200

    - implemented form-section-component

--- src/app/reactive-forms/form-section/form-section.component.ts
+++ src/app/reactive-forms/form-section/form-section.component.ts
@@ -1,15 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit, ChangeDetectorRef, Input } from '@angular/core';
+import { FormSectionUI } from '../../shared/classes/form-section-ui';
 
 @Component({
   selector: 'app-form-section',
   templateUrl: './form-section.component.html',
   styleUrls: ['./form-section.component.scss']
 })
-export class FormSectionComponent implements OnInit {
+export class FormSectionComponent implements OnInit, AfterViewInit {
 
-  constructor() { }
+  @Input() sectionUI: FormSectionUI
+
+  constructor(private cd: ChangeDetectorRef) {
+
+  }
 
   ngOnInit() {
+  }
+
+  //https://stackoverflow.com/a/39787056/249895
+  //https://blog.angularindepth.com/everything-you-need-to-know-about-the-expressionchangedafterithasbeencheckederror-error-e3fd9ce7dbb4
+  ngAfterViewInit() {
+    this.cd.detectChanges();
   }
 
 }
