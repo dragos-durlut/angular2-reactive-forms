7282d2dcc0d0ef9f9136018016426c03ca0ecc04
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-24 13:03:34 +0200

    - added form field UI fields

--- src/app/reactive-forms-inputs/classes/form-field-ui.ts
+++ src/app/reactive-forms-inputs/classes/form-field-ui.ts
@@ -1,7 +1,15 @@
 import { FormField } from '../../shared/interfaces/form-field';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { EventEmitter } from '@angular/core';
 export class FormFieldUI {
   public formField: FormField;
+  public FieldDisplayId: string;
+
+  public FieldFormControl: FormControl;
+  public ParentSectionFormGroup: FormGroup;
+  public MainFormGroup: FormGroup;
+
+  public ValueChange: EventEmitter<any> = new EventEmitter<any>();
 
   constructor(formField: FormField) {
     this.formField = formField;
