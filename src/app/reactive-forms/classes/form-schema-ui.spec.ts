a6705bc67f56119d682280d6a8a6caad9598a65c
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-24 16:43:00 +0200

    - created class form-schema-ui

---
+++ src/app/reactive-forms/classes/form-schema-ui.spec.ts
@@ -0,0 +1,7 @@
+import { FormSchemaUi } from './form-schema-ui';
+
+describe('FormSchemaUi', () => {
+  it('should create an instance', () => {
+    expect(new FormSchemaUi()).toBeTruthy();
+  });
+});
---
+++ src/app/reactive-forms/classes/form-schema-ui.ts
@@ -0,0 +1,14 @@
+import { FormSchema } from '../../shared/interfaces/form-schema';
+import { FormSectionUI } from './form-section-ui';
+import { FormGroup } from '@angular/forms';
+
+export class FormSchemaUi {
+  public formSchema: FormSchema;
+
+  public sectionsUI: Array<FormSectionUI>;
+  public mainFormGroup: FormGroup;
+
+  constructor(formSchema: FormSchema) {
+    this.formSchema = formSchema;
+  }
+}
