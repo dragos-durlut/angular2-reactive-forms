45449e81f867dadb1309510ee18b9e6d2297334c
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-26 00:26:19 +0200

    - removed label with field tuple

--- src/app/reactive-forms-sections/reactive-forms-sections.module.ts
+++ src/app/reactive-forms-sections/reactive-forms-sections.module.ts
@@ -1,20 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsInputsModule } from '../reactive-forms-inputs/reactive-forms-inputs.module';
-import { LabelWithFieldTupleComponent } from './label-with-field-tuple/label-with-field-tuple.component';
 import { FormSectionComponent } from './form-section/form-section.component';
 import { MainFormComponent } from './main-form/main-form.component';
 
 
 
 @NgModule({
-  declarations: [LabelWithFieldTupleComponent, FormSectionComponent, MainFormComponent],
+  declarations: [FormSectionComponent, MainFormComponent],
   imports: [
     CommonModule,
     ReactiveFormsInputsModule
   ],
   exports: [
-    MainFormComponent, LabelWithFieldTupleComponent
+    MainFormComponent
   ]
 })
 export class ReactiveFormsSectionsModule { }
