0b9220705bd1e930b02bc6e120a6c1076b90e7f8
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-26 00:23:02 +0200

    - changed selector

--- src/app/reactive-forms-inputs/field-switcher/field-switcher.component.ts
+++ src/app/reactive-forms-inputs/field-switcher/field-switcher.component.ts
@@ -3,7 +3,7 @@ import { FormFieldTypeEnum } from '../../shared/enums/form-field-type.enum';
 import { BaseInputComponent } from '../classes/base-input.component';
 
 @Component({
-  selector: 'app-field-switcher',
+  selector: '[app-field-switcher]',
   templateUrl: './field-switcher.component.html',
   styleUrls: ['./field-switcher.component.scss']
 })
