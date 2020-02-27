61d18aecdf4ba7fa3fc3bb3528dd2eb2940c1f99
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-27 23:05:07 +0200

    - created forms-example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormFieldUiBuilder } from './reactive-forms-inputs/builders/form-field-ui-builder';
 import { ReactiveFormsSectionsModule } from './reactive-forms-sections/reactive-forms-sections.module';
+import { FormsExampleComponent } from './forms-example/forms-example.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FormsExampleComponent
   ],
   imports: [
     BrowserModule,
