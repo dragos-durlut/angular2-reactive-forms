6b1cdb955e29986963e8a75739f2457275a49945
dragos-durlut <43134494+dragos-durlut@users.noreply.github.com>
2020-02-27 23:10:03 +0200

    - added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { FormsExampleComponent } from './forms-example/forms-example.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'forms-example',
+    component: FormsExampleComponent
+  }
+  ,
+  {
+    path: ''
+    , pathMatch: 'full'
+    , redirectTo: 'forms-example'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
